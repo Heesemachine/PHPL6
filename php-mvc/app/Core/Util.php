efc4dec49ec103552d3a7e21a97c1b95a5ddcb89
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2022-11-13 19:55:01 +0200

    Create Util.php

---
+++ php-mvc/app/Core/Util.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Core;
+
+class Util
+{
+
+    public static function keyValueToList($array = [], $mask = "%s => %s", $separator = ","): string
+    {
+        return implode($separator, array_map("sprintf", array_fill(0, count($array), $mask), array_keys($array), array_values($array)));
+    }
+
+}
